76bc303972ef09ff066ef6202766ecdd776dd577
徐铭 <xumingxs@qq.com>
2022-06-06 16:19:15 +0800

    处理器文件初始化

---
+++ Multiplexer.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/06/06 16:18:32
+// Design Name: 
+// Module Name: Multiplexer32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Multiplexer32(
+    input clk,
+    input rst_n,
+    input [0:4] CE,
+    output dout
+    );
+endmodule
---
+++ Register.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/06/06 16:18:32
+// Design Name: 
+// Module Name: Register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register(
+    input clk,
+    input rst_n,
+    input din,
+    output dout,
+    input EN
+    );
+endmodule
